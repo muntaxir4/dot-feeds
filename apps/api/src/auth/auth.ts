b17fa612a615d290a30479f238114ad2b424617a
muntaxir4 <mjmullick0608@gmail.com>
2024-07-20 15:01:55 +0530

    Init Frontend web

--- apps/api/src/auth/auth.ts
+++ apps/api/src/auth/auth.ts
@@ -1,5 +1,6 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { sign } from "hono/jwt";
+import bcrypt from "bcrypt";
 
 import { PrismaSingleton } from "../db";
 import { setSignedCookie } from "hono/cookie";
@@ -8,22 +9,48 @@ const auth = new Hono<{
   Bindings: { JWT_SECRET: string; COOKIE_SECRET: string };
 }>();
 
+//this is called in both signup and signin
+async function setJwtAndCookie(c: Context, userId: string, username: string) {
+  const payload = {
+    id: userId.substring(0, 5),
+    username,
+  };
+  const token = await sign(payload, process.env.JWT_SECRET as string);
+  await setSignedCookie(
+    c,
+    "token",
+    token,
+    process.env.COOKIE_SECRET as string,
+    {
+      path: "/",
+      secure: true,
+      httpOnly: true,
+      maxAge: 1000,
+    }
+  );
+}
+
 auth.post("/signup", async (c) => {
   const body = await c.req.json();
   const prisma = PrismaSingleton(c);
   try {
     const { email, password, username } = body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
       data: {
         email,
-        password,
+        password: hashedPassword,
         username,
       },
     });
+    await setJwtAndCookie(c, user.id, user.username);
     return c.json({ message: "Signup success", user }, 200);
   } catch (error) {
     console.error(error);
+    if (error?.code === "P2002")
+      return c.json({ message: "Email exists already", error }, 400);
     return c.json({ message: "Signup failed", error }, 400);
   }
 });
@@ -33,34 +60,22 @@ auth.post("/signin", async (c) => {
   const prisma = PrismaSingleton(c);
   try {
     const { email, password } = body;
+
     const user = await prisma.user.findFirst({
       where: {
         email,
-        password,
       },
     });
     if (!user) throw "User not found";
 
-    const payload = {
-      id: user.id.substring(0, 5),
-      username: user.username,
-    };
-    const token = await sign(payload, process.env.JWT_SECRET as string);
-    await setSignedCookie(
-      c,
-      "token",
-      token,
-      process.env.COOKIE_SECRET as string,
-      {
-        path: "/",
-        secure: true,
-        httpOnly: true,
-        maxAge: 1000,
-      }
-    );
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw "Invalid password";
+
+    await setJwtAndCookie(c, user.id, user.username);
     return c.json({ message: "Signin success", user }, 200);
   } catch (error) {
     console.error(error);
+
     return c.json({ message: "Signin failed", error }, 400);
   }
 });
--- packages/database/prisma/migrations/20240719143933_init/migration.sql
+++ packages/database/prisma/migrations/20240719143933_init/migration.sql
@@ -1,16 +1,13 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ALTER COLUMN "id" DROP DEFAULT,
-ALTER COLUMN "id" SET DATA TYPE TEXT,
-ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
-DROP SEQUENCE "User_id_seq";
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Blog" (
@@ -23,6 +20,12 @@ CREATE TABLE "Blog" (
 
     CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
